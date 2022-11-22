0eeecbe6be52826a23640be11d0dfb4cffbbf6e7
Víctor Jiménez <43785784+victor7w7r@users.noreply.github.com>
2022-11-22 13:07:19 -0500

    RemoveUnnecesaryBuilder

--- lib/config/injector.dart
+++ lib/config/injector.dart
@@ -2,16 +2,6 @@ import 'package:kiwi/kiwi.dart';
 
 import 'package:sembast_client_dart/config/app.dart';
 
-part 'injector.g.dart';
-
 final container = KiwiContainer();
 
-void setup() {
-  final injector = _$Injector();
-  injector.configure();
-}
-
-abstract class Injector {
-  @Register.singleton(App)
-  void configure();
-}
+void setup() => container.registerSingleton((_) => App());
